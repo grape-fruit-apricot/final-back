4f8ebeb7ed244a68f3a9457fb7d0f54e60e52711
unknown <ghksl0204@gmail.com>
2026-08-25 17:45:03 +0900

    first commit

---
+++ src/main/java/com/kh/midpoint/MidPointApiApplication.java
@@ -0,0 +1,13 @@
+package com.kh.midpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidPointApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidPointApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/midpoint/MidPointApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.midpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidPointApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
